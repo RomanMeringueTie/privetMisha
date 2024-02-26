d7c354b70ae76ff1f7339df29018615ea3eff333
RomanMeringueTie <RomanMeringueTie@github.com>
2024-02-27 00:41:17 +0700

    Fix header format

--- include/mySimpleComputer.h
+++ include/mySimpleComputer.h
@@ -1,12 +1,9 @@
 #pragma once
-#include <limits.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 extern int *ram;
 
@@ -16,46 +13,46 @@ extern int command;
 
 extern int flag;
 
-int sc_memoryInit(void);
+int sc_memoryInit (void);
 
-int sc_memorySet(int address, int value);
+int sc_memorySet (int address, int value);
 
-int sc_memoryGet(int address, int *value);
+int sc_memoryGet (int address, int *value);
 
-int sc_memorySave(char *filename);
+int sc_memorySave (char *filename);
 
-int sc_memoryLoad(char *filename);
+int sc_memoryLoad (char *filename);
 
-int sc_regInit(void);
+int sc_regInit (void);
 
-int sc_regSet(int reg, int value);
+int sc_regSet (int reg, int value);
 
-int sc_regGet(int reg, int *value);
+int sc_regGet (int reg, int *value);
 
-int sc_accumulatorInit(void);
+int sc_accumulatorInit (void);
 
-int sc_accumulatorSet(int value);
+int sc_accumulatorSet (int value);
 
-int sc_accumulatorGet(int *value);
+int sc_accumulatorGet (int *value);
 
-int sc_icounterInit(void);
+int sc_icounterInit (void);
 
-int sc_icounterSet(int value);
+int sc_icounterSet (int value);
 
-int sc_icounterGet(int *value);
+int sc_icounterGet (int *value);
 
-int sc_commandEncode(int sign, int command, int operand, int *value);
+int sc_commandEncode (int sign, int command, int operand, int *value);
 
-int sc_commandDecode(int value, int *sign, int *command, int *operand);
+int sc_commandDecode (int value, int *sign, int *command, int *operand);
 
-int sc_commandValidate(int command);
+int sc_commandValidate (int command);
 
-void printCell(int address);
+void printCell (int address);
 
-void printFlags(void);
+void printFlags (void);
 
-void printDecodedCommand(int value);
+void printDecodedCommand (int value);
 
-void printAccumulator(void);
+void printAccumulator (void);
 
-void printCounters(void);
+void printCounters (void);
